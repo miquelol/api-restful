3831ea5fa915dd2a2bfffee90e7b0b4297e996ab
Antonio Miquelino <antonio_miquelino@hotmail.com>
2023-10-25 07:34:58 -0300

    adicionando model e controller de produtos

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,21 @@
 const router = require('express').Router()
+const ProductController = require('../controllers/products')
 
-router.get('/clientes', (req,res) => {
-    res.send({
-        ok: 123
-    })
-})
+
+// VERBOS HTTP (4 tipos)
+//GET - Obter dados
+//POST - enviar/receber dados
+//PUT - atualizar
+//DELETE - deletar
+
+
+router.get('/products', ProductController.get)
+//router.post('/products', ProductController.post)
+//router.put('/products', ProductController.put)
+//router.delete('/products', ProductController.delete)
+
+
+
 
 
 module.exports = router
